aa9782cd539123ccf7ecd14570a7709e84129b24
Thang Nguyen <nguyenquocthang16022000@gmail.com>
2021-11-29 17:28:36 +0700

    update class mon hoc

--- Repositories/MonHocDAL.cs
+++ Repositories/MonHocDAL.cs
@@ -1,6 +1,4 @@
-﻿using Dapper;
-using StudentManagement.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,27 +6,7 @@ using System.Threading.Tasks;
 
 namespace StudentManagement.Repositories
 {
-    public class MonHocDAL
+    class MonHocDAL
     {
-        public DataResponse<List<MONHOC>> GetListMonHoc()
-        {
-            if (!BaseDAl.Connect())
-                return new DataResponeFail<List<MONHOC>>("Lỗi kết nối");
-            try
-            {
-                string command = "exec dbo.SP_DS_MonHoc";
-                var data = Program.conn.Query<MONHOC>(command).ToList();
-                return new DataResponeSuccess<List<MONHOC>>(data);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return new DataResponeFail<List<MONHOC>>("Lỗi hệ thống");
-            }
-            finally
-            {
-                BaseDAl.DisConnect();
-            }
-        }
     }
 }
---
+++ Repositories/SinhVienDAL.cs
@@ -0,0 +1,102 @@
+﻿using Dapper;
+using DapperParameters;
+using StudentManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Repositories
+{
+    class SinhVienDAL
+    {
+        public DataResponse<List<SINHVIEN>> GetListSinhVien()
+        {
+            if (!BaseDAl.Connect())
+                return new DataResponeFail<List<SINHVIEN>>("Lỗi kết nối");
+            try
+            {
+                string command = "exec dbo.SP_DS_SINHVIEN";
+                var data = Program.conn.Query<SINHVIEN>(command).ToList();
+                return new DataResponeSuccess<List<SINHVIEN>>(data);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new DataResponeFail<List<SINHVIEN>>("Lỗi hệ thống");
+            }
+            finally
+            {
+                BaseDAl.DisConnect();
+            }
+        }
+        public DataResponse<List<SINHVIEN>> GetListSinhVienByLop(string idClass)
+        {
+            if (!BaseDAl.Connect())
+                return new DataResponeFail<List<SINHVIEN>>("Lỗi kết nối");
+            try
+            {
+                string command = "exec dbo.SP_DS_SINHVIEN_BY_LOP @MALOP";
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@MALOP", idClass);
+                var data = Program.conn.Query<SINHVIEN>(command,parameters).ToList();
+                return new DataResponeSuccess<List<SINHVIEN>>(data);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new DataResponeFail<List<SINHVIEN>>("Lỗi hệ thống");
+            }
+            finally
+            {
+                BaseDAl.DisConnect();
+            }
+        }
+
+        public DataResponse<bool> UpdateSinhVien(List<UPDATESINHVIEN> list)
+        {
+            if (!BaseDAl.Connect())
+                return new DataResponeFail<bool>("Lỗi kết nối");
+            try
+            {
+                string command = "exec [dbo].[SP_UPDATE_SINHVIEN] @SINHVIEN";
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.AddTable("@SINHVIEN", "TYPE_NEWUPDATE_SINHVIEN", list);
+                Program.conn.Execute(command, parameters);
+                return new DataResponeSuccess<bool>(true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new DataResponeFail<bool>("Lỗi hệ thống");
+            }
+            finally
+            {
+                BaseDAl.DisConnect();
+            }
+        }
+        public DataResponse<bool> CheckSinhvien(string masv)
+        {
+            if (!BaseDAl.Connect())
+                return new DataResponeFail<bool>("Lỗi kết nối");
+            try
+            {
+                string command = "select [dbo].[func_KT_SINHVIEN_EXISTS] (@MASV)";
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@MASV", masv);
+                var res = Program.conn.ExecuteScalar<bool>(command, parameters);
+                return new DataResponeSuccess<bool>(res);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new DataResponeFail<bool>("Lỗi hệ thống");
+            }
+            finally
+            {
+                BaseDAl.DisConnect();
+            }
+        }
+    }
+}
