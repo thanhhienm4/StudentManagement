320ae2f52f4f5a0d67b4c719b4bfb19fdd81322a
tranquocbao <tranquocb306@gmail.com>
2021-12-08 23:28:56 +0700

    hiden password

--- UCAddLogin.Designer.cs
+++ UCAddLogin.Designer.cs
@@ -46,7 +46,8 @@ namespace StudentManagement
             // 
             // lkGiangVien
             // 
-            this.lkGiangVien.Location = new System.Drawing.Point(401, 212);
+            this.lkGiangVien.Location = new System.Drawing.Point(312, 179);
+            this.lkGiangVien.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.lkGiangVien.Name = "lkGiangVien";
             this.lkGiangVien.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
@@ -56,76 +57,85 @@ namespace StudentManagement
             this.lkGiangVien.Properties.DisplayMember = "HOTEN";
             this.lkGiangVien.Properties.NullText = "";
             this.lkGiangVien.Properties.ValueMember = "MAGV";
-            this.lkGiangVien.Size = new System.Drawing.Size(236, 26);
+            this.lkGiangVien.Size = new System.Drawing.Size(184, 22);
             this.lkGiangVien.TabIndex = 21;
             // 
             // cbxRole
             // 
-            this.cbxRole.Location = new System.Drawing.Point(401, 270);
+            this.cbxRole.Location = new System.Drawing.Point(312, 227);
+            this.cbxRole.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.cbxRole.Name = "cbxRole";
             this.cbxRole.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.cbxRole.Size = new System.Drawing.Size(236, 26);
+            this.cbxRole.Size = new System.Drawing.Size(184, 22);
             this.cbxRole.TabIndex = 20;
             // 
             // Tạo
             // 
-            this.Tạo.Location = new System.Drawing.Point(401, 368);
+            this.Tạo.Location = new System.Drawing.Point(312, 310);
+            this.Tạo.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Tạo.Name = "Tạo";
-            this.Tạo.Size = new System.Drawing.Size(236, 34);
+            this.Tạo.Size = new System.Drawing.Size(184, 29);
             this.Tạo.TabIndex = 19;
             this.Tạo.Text = "Tạo tài khoản";
             this.Tạo.Click += new System.EventHandler(this.Tạo_Click);
             // 
             // labelControl4
             // 
-            this.labelControl4.Location = new System.Drawing.Point(132, 273);
+            this.labelControl4.Location = new System.Drawing.Point(103, 230);
+            this.labelControl4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.labelControl4.Name = "labelControl4";
-            this.labelControl4.Size = new System.Drawing.Size(46, 19);
+            this.labelControl4.Size = new System.Drawing.Size(41, 17);
             this.labelControl4.TabIndex = 18;
             this.labelControl4.Text = "Quyền";
             // 
             // lbGiangVien
             // 
-            this.lbGiangVien.Location = new System.Drawing.Point(132, 219);
+            this.lbGiangVien.Location = new System.Drawing.Point(103, 184);
+            this.lbGiangVien.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.lbGiangVien.Name = "lbGiangVien";
-            this.lbGiangVien.Size = new System.Drawing.Size(71, 19);
+            this.lbGiangVien.Size = new System.Drawing.Size(62, 17);
             this.lbGiangVien.TabIndex = 17;
             this.lbGiangVien.Text = "Đối tượng";
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(132, 160);
+            this.labelControl2.Location = new System.Drawing.Point(103, 135);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(64, 19);
+            this.labelControl2.Size = new System.Drawing.Size(56, 17);
             this.labelControl2.TabIndex = 16;
             this.labelControl2.Text = "Mật khẩu";
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(132, 98);
+            this.labelControl1.Location = new System.Drawing.Point(103, 83);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(107, 19);
+            this.labelControl1.Size = new System.Drawing.Size(93, 17);
             this.labelControl1.TabIndex = 15;
             this.labelControl1.Text = "Tên đăng nhập";
             // 
             // tePW
             // 
-            this.tePW.Location = new System.Drawing.Point(401, 153);
+            this.tePW.Location = new System.Drawing.Point(312, 129);
+            this.tePW.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.tePW.Name = "tePW";
-            this.tePW.Size = new System.Drawing.Size(236, 26);
+            this.tePW.Properties.UseSystemPasswordChar = true;
+            this.tePW.Size = new System.Drawing.Size(184, 22);
             this.tePW.TabIndex = 14;
             // 
             // teLogin
             // 
-            this.teLogin.Location = new System.Drawing.Point(401, 95);
+            this.teLogin.Location = new System.Drawing.Point(312, 80);
+            this.teLogin.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.teLogin.Name = "teLogin";
-            this.teLogin.Size = new System.Drawing.Size(236, 26);
+            this.teLogin.Size = new System.Drawing.Size(184, 22);
             this.teLogin.TabIndex = 13;
             // 
             // UCAddLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.lkGiangVien);
             this.Controls.Add(this.cbxRole);
@@ -136,8 +146,9 @@ namespace StudentManagement
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.tePW);
             this.Controls.Add(this.teLogin);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "UCAddLogin";
-            this.Size = new System.Drawing.Size(769, 496);
+            this.Size = new System.Drawing.Size(598, 418);
             ((System.ComponentModel.ISupportInitialize)(this.lkGiangVien.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cbxRole.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tePW.Properties)).EndInit();
